3a3c124cf96c063cbf5c26b1e1bd29ab61554024
Felix Eurich <felix.eurich@novatec-gmbh.de>
2018-02-23 16:44:22 +0100

    Added ObjectReferences and Attachments to task

--- performance-test/src/main/java/pro/taskana/data/generation/builder/TaskBuilder.java
+++ performance-test/src/main/java/pro/taskana/data/generation/builder/TaskBuilder.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import pro.taskana.data.generation.util.ClassificationType;
 import pro.taskana.data.generation.util.TaskWrapper;
 import pro.taskana.impl.ClassificationImpl;
-import pro.taskana.impl.ObjectReference;
 import pro.taskana.impl.TaskImpl;
 import pro.taskana.impl.TaskState;
 import pro.taskana.impl.WorkbasketImpl;
@@ -17,21 +17,33 @@ public class TaskBuilder {
     
     private List<ClassificationImpl> taskClassifications;
     private Random rnd;
-    
     private List<WorkbasketImpl> affectedWorkbaskets;
     private Map<TaskState, Integer> taskDistribution;
     
-    public TaskBuilder(List<ClassificationImpl> taskClassifications) {
+    private AttachmentBuilder attachmentBuilder;
+    private ObjectReferenceBuilder objectReferenceBuilder;
+    
+    //currently unused because there is no possiblity to map object references to task yet.
+    private int minNumOfObjectReferences;
+    private int maxNumOfObjectReferences;
+    private int numberOfAttachments;
+    
+    public TaskBuilder(Map<ClassificationType, List<ClassificationImpl>> classifications) {
         this.taskDistribution = new HashMap<>();
         this.affectedWorkbaskets = new ArrayList<>();
-        this.taskClassifications = taskClassifications;
+        this.taskClassifications = classifications.get(ClassificationType.AUFGABENTYP);
         this.rnd = new Random();
+        attachmentBuilder = new AttachmentBuilder(classifications);
+        objectReferenceBuilder = new ObjectReferenceBuilder(200);
     }
     
     
     public TaskBuilder affect(List<WorkbasketImpl> workbaskets) {
         this.affectedWorkbaskets = workbaskets;
         this.taskDistribution = new HashMap<>();
+        this.maxNumOfObjectReferences = 1;
+        this.minNumOfObjectReferences = 1;
+        this.numberOfAttachments = 0;
         return this;
     }
     
@@ -41,6 +53,23 @@ public class TaskBuilder {
             tasks += taskDistribution.get(state);
         }
         taskDistribution.put(state, tasks);
+        return this;
+    }
+    
+    public TaskBuilder withObjectReferences(int exactNumberOfObjectReferences) {
+        minNumOfObjectReferences = exactNumberOfObjectReferences;
+        maxNumOfObjectReferences = exactNumberOfObjectReferences;
+        return this;
+    }
+    
+    public TaskBuilder withObjectReferences(int minNumberOfObjectReferences, int maxNumberOfObjectReferences) {
+        this.minNumOfObjectReferences = minNumberOfObjectReferences;
+        this.maxNumOfObjectReferences = maxNumberOfObjectReferences;
+        return this;
+    }
+    
+    public TaskBuilder withAttachments(int numberOfAttachments) {
+        this.numberOfAttachments = numberOfAttachments;
         return this;
     }
     
@@ -73,24 +102,14 @@ public class TaskBuilder {
             ClassificationImpl taskClassification = taskClassifications.get(rndIndex);
             String classificationKey = taskClassification.getKey();
             task.setClassificationKey(classificationKey);
-            task.setPrimaryObjRef(createObjectReference("Company A", "System A", "Instance A", "VNR", "1234567"));
-            
+            task.setPrimaryObjRef(objectReferenceBuilder.getObjectReference());
+            task.setAttachments(attachmentBuilder.getAttachments(numberOfAttachments));
             tasks.add(task);
         }
         return tasks;
     }
     
-     private ObjectReference createObjectReference(String company, String system, String systemInstance, String type,
-        String value) {
-        ObjectReference objectReference = new ObjectReference();
-        objectReference.setCompany(company);
-        objectReference.setSystem(system);
-        objectReference.setSystemInstance(systemInstance);
-        objectReference.setType(type);
-        objectReference.setValue(value);
-        
-        return objectReference;
-    }
+
     
 
 }
--- performance-test/src/main/java/pro/taskana/data/generation/util/ElementStack.java
+++ performance-test/src/main/java/pro/taskana/data/generation/util/ElementStack.java
@@ -11,55 +11,65 @@ import java.util.List;
  */
 public class ElementStack<E> {
 
-	private List<E> elements;
+    private List<E> elements;
 
-	public ElementStack() {
-		elements = new ArrayList<>();
-	}
+    public ElementStack() {
+        elements = new ArrayList<>();
+    }
 
-	public ElementStack(List<E> elements) {
-		this.elements = elements;
-	}
+    public ElementStack(List<E> elements) {
+        this.elements = elements;
+    }
 
-	/**
-	 * Checks if the contained list is null or empty.
-	 * 
-	 * @return <code>true</code> if the contained list is null or empty, otherwise
-	 *         <code>false</code>
-	 */
-	public boolean isEmpty() {
-		return elements == null || elements.isEmpty();
-	}
+    /**
+     * Adds a new item to the wrapped list.
+     * 
+     * @param item
+     *            to be added.
+     */
+    public void add(E item) {
+        this.elements.add(item);
+    }
 
-	/**
-	 * Removes a number of elements starting at index 0 and return this as list.
-	 * 
-	 * @param amount
-	 *            number of elements which will be removed from the list
-	 * @return list of removed elements
-	 */
-	public List<E> pop(int amount) {
-		List<E> subList = elements.subList(0, amount);
-		elements = elements.subList(amount, elements.size());
-		return subList;
-	}
+    /**
+     * Checks if the contained list is null or empty.
+     * 
+     * @return <code>true</code> if the contained list is null or empty, otherwise
+     *         <code>false</code>
+     */
+    public boolean isEmpty() {
+        return elements == null || elements.isEmpty();
+    }
 
-	/**
-	 * Supplies the number of elements in the stack.
-	 * 
-	 * @return size of the stack
-	 */
-	public int getSize() {
-		return elements.size();
-	}
+    /**
+     * Removes a number of elements starting at index 0 and return this as list.
+     * 
+     * @param amount
+     *            number of elements which will be removed from the list
+     * @return list of removed elements
+     */
+    public List<E> pop(int amount) {
+        List<E> subList = elements.subList(0, amount);
+        elements = elements.subList(amount, elements.size());
+        return subList;
+    }
 
-	/**
-	 * Supplies a list representation for the contained elements.
-	 * 
-	 * @return List of elements
-	 */
-	public List<E> toList() {
-		return elements;
-	}
+    /**
+     * Supplies the number of elements in the stack.
+     * 
+     * @return size of the stack
+     */
+    public int getSize() {
+        return elements.size();
+    }
+
+    /**
+     * Supplies a list representation for the contained elements.
+     * 
+     * @return List of elements
+     */
+    public List<E> toList() {
+        return elements;
+    }
 
 }
