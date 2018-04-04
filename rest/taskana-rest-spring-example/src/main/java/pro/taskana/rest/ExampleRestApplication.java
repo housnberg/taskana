b9a0e355aacdf5487400eb8f9c26e9436aa84a73
Felix Eurich <felix.eurich@novatec-gmbh.de>
2018-04-04 12:19:54 +0200

    Improvements

--- rest/taskana-rest-spring-example/src/main/java/pro/taskana/rest/ExampleRestApplication.java
+++ rest/taskana-rest-spring-example/src/main/java/pro/taskana/rest/ExampleRestApplication.java
@@ -1,22 +1,9 @@
 package pro.taskana.rest;
 
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import pro.taskana.sampledata.SampleDataGenerator;
 
 /**
  * Example Application showing the implementation of taskana-rest-spring.
@@ -30,30 +17,4 @@ public class ExampleRestApplication {
         SpringApplication.run(ExampleRestApplication.class, args);
     }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "datasource")
-    public DataSourceProperties dataSourceProperties() {
-        DataSourceProperties props = new DataSourceProperties();
-        props.setUrl("jdbc:h2:mem:taskana;IGNORECASE=TRUE;LOCK_MODE=0;INIT=CREATE SCHEMA IF NOT EXISTS TASKANA");
-        return props;
-    }
-
-    @Bean
-    public DataSource dataSource(DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().build();
-    }
-
-    @Bean
-    public PlatformTransactionManager txManager(DataSource dataSource) {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    @Bean
-    @DependsOn("taskanaEngineConfiguration") // generate sample data after schema was inserted
-    public SampleDataGenerator generateSampleData(DataSource dataSource) throws SQLException {
-        SampleDataGenerator sampleDataGenerator = new SampleDataGenerator(dataSource);
-        sampleDataGenerator.generateSampleData();
-        return sampleDataGenerator;
-    }
 }
